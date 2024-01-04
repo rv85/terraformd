08461fd9ded39ac65d4f494d874beeddd1e7017d
rv85 <ravinder.gugloth@gmail.com>
2024-01-04 15:19:28 +0000

    files changed

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource "aws_instance" "ec2-demo" {
+    ami =""
+    instance_type = "t2.micro"
+  
+}
---
+++ version.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "0.14.5"
+  required_providers {
+    aws = {
+        version = "5.13.0"
+        source = "hashicorp/aws"
+    }
+  }
+
+  #backend state file
+  backend "s3" {
+    bucket="my-ec2-bucket-ravi"
+    region= "eu-west-2"
+    key="logfiles"
+    
+  }
+}
